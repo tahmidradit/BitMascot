d19a1b05b3735de1a54c6f7b64725eaa526d000f
tahmidradit <tahmid.radit@outlook.com>
2021-09-30 07:23:51 +0600

    Bootstrap link modified

--- Views/registerUser.php
+++ Views/registerUser.php
@@ -1,4 +1,5 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+<link href="../lib/bootstrap-5.1.1-dist/css/bootstrap.min.css" rel="stylesheet">
 
 <br> <br>
 <div align="center" class=" mt-4">
