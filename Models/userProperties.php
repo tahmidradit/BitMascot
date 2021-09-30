30448e4ca6f716eff93885387ae4cd9fb7f6b5dc
tahmidradit <tahmid.radit@outlook.com>
2021-09-30 07:13:02 +0600

    Registration page UI modified

--- Models/userProperties.php
+++ Models/userProperties.php
@@ -3,14 +3,14 @@
     function userProperties() {
         global $firstName, $lastName, $address, $phone, $email, $dob, $age, $password;
 
-        $firstName             = $_POST['firstName'];
-        $lastName       = $_POST['lastName'];
-        $address           = $_POST['address'];
-        $phone       = $_POST['phone'];
-        $email     = $_POST['email'];
-        $dob    = $_POST['dob'];
-        $age    = $_POST['age'];
-        $password    = $_POST['password'];
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        $address = $_POST['address'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $dob = $_POST['dob'];
+        $age = $_POST['age'];
+        $password = $_POST['password'];
     }
     
 ?>
--- Views/registerUser.php
+++ Views/registerUser.php
@@ -1,85 +1,88 @@
-
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-<div class="card text-white">
-    <div class="card-header bg-primary">Register New User</div>
-    <div class="card-body">
-        <form  id="form">
-            <div class="form-group row">
-                <div class="col-2 text-primary">
-                    <label>First Name</label>
+<br> <br>
+<div align="center" class=" mt-4">
+    <div class="card text-white" style="width: 70%;">
+        <div class="card-header bg-primary">Register New User</div>
+        <div class="card-body">
+            <form id="form">
+                <div class="form-group row">
+                    <div class="col-4 text-primary">
+                        <label>First Name</label>
+                    </div>
+                    <div class="col-6">
+                        <input class="form-control" type="text" name="firstName" placeholder="First Name" id="firstName" />
+                    </div>
                 </div>
-                <div class="col-4">
-                    <input class="form-control" type="text" name="firstName" placeholder="First Name" id="firstName" />
+                <div class="form-group row">
+                    <div class="col-4 mt-2 text-primary">
+                        <label>Last Name</label>
+                    </div>
+                    <div class="col-6 mt-2">
+                        <input class="form-control" type="text" name="lastName" placeholder="Last Name" id="lastName" />
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-2 mt-2 text-primary">
-                    <label>Last Name</label>
+                <div class="form-group row">
+                    <div class="col-4 mt-2 text-primary">
+                        <label>Address</label>
+                    </div>
+                    <div class="col-6 mt-2">
+                        <input class="form-control" type="text" name="address" placeholder="Address" id="address" />
+                    </div>
                 </div>
-                <div class="col-4 mt-2">
-                    <input class="form-control" type="text" name="lastName" placeholder="Last Name" id="lastName" />
+                <div class="form-group row">
+                    <div class="col-4 mt-2 text-primary">
+                        <label>Phone</label>
+                    </div>
+                    <div class="col-6 mt-2">
+                        <input class="form-control" type="text" name="phone" placeholder="Phone" id="phone" />
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-2 mt-2 text-primary">
-                    <label>Address</label>
+                <div class="form-group row">
+                    <div class="col-4 mt-2 text-primary">
+                        <label>Email</label>
+                    </div>
+                    <div class="col-6 mt-2">
+                        <input class="form-control" type="text" name="email" placeholder="Email" id="email" />
+                    </div>
                 </div>
-                <div class="col-4 mt-2">
-                    <input class="form-control" type="text" name="address" placeholder="Address" id="address" />
+                <div class="form-group row">
+                    <div class="col-4 mt-2 text-primary">
+                        <label>Birthdate</label>
+                    </div>
+                    <div class="col-6 mt-2">
+                        <input class="form-control" type="date" name="dob" placeholder="Dob" id="dob" />
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-2 mt-2 text-primary">
-                    <label>Phone</label>
+                <div class="form-group row">
+                    <div class="col-4 mt-2 text-primary">
+                        <label>Age</label>
+                    </div>
+                    <div class="col-6 mt-2">
+                        <input class="form-control" type="text" name="age" placeholder="Age" id="age" />
+                    </div>
                 </div>
-                <div class="col-4 mt-2">
-                    <input class="form-control" type="text" name="phone" placeholder="Phone" id="phone" />
+                <div class="form-group row">
+                    <div class="col-4 mt-2 text-primary">
+                        <label>Password</label>
+                    </div>
+                    <div class="col-6 mt-2">
+                        <input class="form-control" type="password" name="password" placeholder="Password" id="password" />
+                    </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-2 mt-2 text-primary">
-                    <label>Email</label>
+                <div class="form-group row">
+                    <div class="col-4 mt-2 text-primary">
+
+                    </div>
+                    <div class="col-6 mt-2">
+                        <input class="form-control btn btn-primary text-white" type="button" id="register" value="Submit" />
+                    </div>
                 </div>
-                <div class="col-4 mt-2">
-                    <input class="form-control" type="text" name="email" placeholder="Email" id="email" />
-                </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-2 mt-2 text-primary">
-                    <label>Birthdate</label>
-                </div>
-                <div class="col-4 mt-2">
-                    <input class="form-control" type="date" name="dob" placeholder="Dob" id="dob" />
-                </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-2 mt-2 text-primary">
-                    <label>Age</label>
-                </div>
-                <div class="col-4 mt-2">
-                <input class="form-control" type="text" name="age" placeholder="Age" id="age" />
-                </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-2 mt-2 text-primary">
-                    <label>Password</label>
-                </div>
-                <div class="col-4 mt-2">
-                <input class="form-control" type="password" name="password" placeholder="Password" id="password" />
-                </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-2 mt-2 text-primary">
-                
-                </div>
-                <div class="col-4 mt-2">
-                <input class="form-control btn btn-primary text-white" type="button" id="register" value="Submit" />
-                </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 </div>
+
 
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="../lib/js/registerUser.js"></script>
