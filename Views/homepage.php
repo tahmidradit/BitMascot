db2c77f0a9fb4f470025626c945f39919b7fb171
tahmidradit <tahmid.radit@outlook.com>
2021-10-01 08:32:49 +0600

    homepage redesigned and removed unnecessary files

---
+++ Views/homepage.php
@@ -0,0 +1,34 @@
+<?php include 'registerUser.php';
+?>
+<html>
+<head>
+    <title>Home - Bit Mascot</title>
+</head>
+<body>
+    <link rel="stylesheet" href="../lib/css/site.css">
+    <link href="../lib/bootstrap-5.1.1-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <header class="customHeader">
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+            <div class="container-fluid">
+            <a href="homepage.php"> <img align="left" src="../lib/images/BitMascot.png" /> </a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active text-white" aria-current="page" href="homepage.php"> &nbsp; Home</a>
+                  </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a class="text-white" href="../Views/registerUser.php">Registration</a></li> &nbsp;
+                    <li><a class="text-white" href="../Views/login.php">Login</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </header>
+    <img src="../lib/images/office.jpg" style=" width: fit-content; left: 0;" />
+</body>
+</html>
