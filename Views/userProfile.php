cdeceaeb796473bc3cd346d7749f0a5418331dab
tahmidradit <tahmid.radit@outlook.com>
2021-10-01 08:01:53 +0600

    User info in table

--- Views/userProfile.php
+++ Views/userProfile.php
@@ -1,57 +1,25 @@
 
 <?php
-session_start();
-require_once('../Models/dbConnection.php');
+  session_start();
+  require_once('../Models/dbConnection.php');
 ?>
 <link href="../lib/bootstrap-5.1.1-dist/css/bootstrap.min.css" rel="stylesheet">
-<nav class="navbar navbar-expand-lg navbar-light bg-primary">
-            <div class="container-fluid">
-              <a class="navbar-brand text-white" href="#">Navbar</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="homepage.html">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Link</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <li><a class="dropdown-item text-white" href="#">Action</a></li>
-                      <li><a class="dropdown-item text-white" href="#">Another action</a></li>
-                      <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                  </li>
-                </ul>
-                <form class="d-flex">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-success" type="submit">Search</button>
-                </form>
-              </div>
-            </div>
-          </nav>
 
-          <?php 
-            global $row;
-            $_email = $_SESSION['email'];
-            $mysqlConnection = mySqlConnection();
-            $query = "SELECT * from users where Email ='$_email'";
-            $sqlExecution = mysqli_query($mysqlConnection, $query);
-            $row = mysqli_fetch_array($sqlExecution);
-            ?>
+<?php 
+  global $row;
+  $_email = $_SESSION['email'];
+  $mysqlConnection = mySqlConnection();
+  $query = "SELECT * from users where Email ='$_email'";
+  $sqlExecution = mysqli_query($mysqlConnection, $query);
+  $row = mysqli_fetch_array($sqlExecution);
+?>
 
 <div class="container-fluid">
     <div class="row flex-nowrap">
         <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
             <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-            <h4 class="fs-5 d-none d-sm-inline badge bg-primary text-white"> <?php echo $row['FirstName'] ; echo "&nbsp;"; echo $row['LastName']; ?> </h4>
+            <h4 class="fs-5 d-none d-sm-inline badge bg-white text-dark form-control"> <?php echo "Dashboard" ?> </h4>
+            <h4 class="fs-5 d-none d-sm-inline form-control badge bg-primary text-white"> <?php echo $row['FirstName'] ; echo "&nbsp;"; echo $row['LastName']; ?> </h4> 
 
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                     <li class="nav-item">
@@ -71,23 +39,44 @@ require_once('../Models/dbConnection.php');
                 </ul>
                 <hr>
             </div>
-        </div>
+</div>
         <div class="col py-3">
+          <br> <br>
+          <center>
+          <h4 class="fs-5 d-none d-sm-inline form-control badge bg-primary text-white"> <?php echo "User Profile"; ?> </h4>
+          </center>
+          <br> <br>
+
         <div class="mt-2">
             <table class="table table-hover table-bordered table-sm text-center" style="background-color:lightskyblue" width="100%">
-                <thead>
+                <thead align="vertical">
                      <tr>
                         <th>Id</th>
                         <th>User Name</th>
-                        <th>User Name</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Address</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                        <th>Birthdate</th>
+                        <th>Age</th>
                      </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td> </td>
+                        <td><?php echo $row['Id'];?></td>
+                        <td><?php echo $row['UserName'];?></td>
+                        <td><?php echo $row['FirstName'];?></td>
+                        <td><?php echo $row['LastName'];?></td>
+                        <td><?php echo $row['Address'];?></td>
+                        <td><?php echo $row['Phone'];?></td>
+                        <td><?php echo $row['Email'];?></td>
+                        <td><?php echo $row['Birthdate'];?></td>
+                        <td><?php echo $row['Age'];?></td>
                     </tr>
                 </tbody>
             </table>
+
         </div>  
         </div>
     </div>
